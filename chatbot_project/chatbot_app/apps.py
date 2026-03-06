b5571a1b5c699f76cee1ebe7a2e56325b21734c6
Monisha <monishass636655@gmail.com>
2026-03-06 11:18:29 +0530

    md

--- chatbot_project/chatbot_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatbotAppConfig(AppConfig):
-    name = 'chatbot_app'
--- chatbot_project/chatbot_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-def chatbot(request):
-    return HttpResponse("Hello Monisha! This is my chatbot running in Django.")
