cb6942d92499b71683c97a089389eec8fc641af3
Monisha <monishass636655@gmail.com>
2026-03-06 09:53:32 +0530

    md

--- chatbot_project/chatbot_app/views.py
+++ chatbot_project/chatbot_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def chatbot(request):
+    return HttpResponse("Hello Monisha! This is my chatbot running in Django.")
